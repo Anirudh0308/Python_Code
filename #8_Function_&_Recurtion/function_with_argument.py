f628e520c78deb07beb6783c76a43fcf0c7a2d9e
Anirudh0308 <anirudhgpj360@gmail.com>
2025-04-10 11:41:50 +0530

    function and recurtion

---
+++ #8_Function_&_Recurtion/function_with_argument.py
@@ -0,0 +1,11 @@
+def goodDay(name):
+    print("Good Day "+ name)
+
+goodDay("Anirudh")
+
+
+def goodDay(name, ending):
+    print("Good Day "+ name)
+    print(ending)
+
+goodDay("Anirudh", "Thankyou")
---
+++ #8_Function_&_Recurtion/recurtion.py
@@ -0,0 +1,9 @@
+#factorial of number using recurtion
+
+def factorial(n):
+    if(n==1 or n==0):
+        return 1
+    return n * factorial(n-1)
+
+n = int(input("Enter a number: "))
+print(f"The factorial of {n} is {factorial(n)}")
